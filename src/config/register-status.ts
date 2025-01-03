e4db3810574ed59157a549fb9c5187e8af39eaf8
Nguyễn Quang Sáng <quangsang@192.168.1.2.non-exists.ptr.local>
2025-01-03 14:07:58 +0700

    register-without-admin

---
+++ src/config/register-status.ts
@@ -0,0 +1,1 @@
+export const registerStatus = new Map<string, boolean>();
--- src/services/admin/staff-manage.ts
+++ src/services/admin/staff-manage.ts
@@ -12,3 +12,15 @@ export const getAllStaffs = async () => {
         client.release()
     }
 }
+
+export const addStaff = async (staff: any) => {
+    const client = await pool.connect()
+    try {
+        await client.query("INSERT INTO staffs (full_name, role_name, phone_number, company_mail) VALUES ($1, $2, $3, $4)", [staff.full_name, staff.role_name, staff.phone_number, staff.company_mail])
+    } catch (err) {
+        console.error("Error adding staff:", err)
+        throw err
+    } finally {
+        client.release()
+    }
+}
