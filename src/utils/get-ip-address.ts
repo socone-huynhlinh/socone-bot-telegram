40b2cb07942380582f4caeae3a500d541e7f7598
yorichi <hoangnguyen241003@gmail.com>
2025-01-22 14:06:27 +0700

    update ip

--- src/utils/get-ip-address.ts
+++ src/utils/get-ip-address.ts
@@ -1,15 +1,16 @@
-import os from 'os';
+import os from "os"
 
 function getLocalIp() {
-  const networkInterfaces = os.networkInterfaces();
-  for (const interfaceName in networkInterfaces) {
-    for (const iface of networkInterfaces[interfaceName] || []) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address; // Địa chỉ IPv4 không phải localhost
-      }
-    }
-  }
-  return '127.0.0.1'; // Mặc định trả về localhost nếu không tìm thấy
+    // const networkInterfaces = os.networkInterfaces();
+    // for (const interfaceName in networkInterfaces) {
+    //   for (const iface of networkInterfaces[interfaceName] || []) {
+    //     if (iface.family === 'IPv4' && !iface.internal) {
+    //       return iface.address; // Địa chỉ IPv4 không phải localhost
+    //     }
+    //   }
+    // }
+    // return '127.0.0.1'; // Mặc định trả về localhost nếu không tìm thấy
+    return process.env.VPS_IP
 }
 
-export default getLocalIp;
+export default getLocalIp
